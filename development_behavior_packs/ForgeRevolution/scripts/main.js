a8ff0bfd85a77fa76028f9399d0cc58700c2326e
WaldsonLima <waldsonh@gmail.com>
2024-12-19 18:27:03 -0300

    Adicionando efeitos nos aneis

---
+++ development_behavior_packs/ForgeRevolution/scripts/main.js
@@ -0,0 +1,33 @@
+import * as Minecraft from "@minecraft/server";
+
+const amethystRingComponent = {
+    onUse(event) {
+        const player = event.source // The entity that used the item on the block.
+        const act = event.itemStack // The block permutation that the item was used onw.
+        
+        if (player && act) {
+            //Minecraft.world.sendMessage("Efeito da ametista aplicado");
+            player.runCommand("/effect @s regeneration 30 1");
+        }
+    }
+};
+
+Minecraft.world.beforeEvents.worldInitialize.subscribe(({ itemComponentRegistry }) => {
+    itemComponentRegistry.registerCustomComponent("forge:amethyst_ring", amethystRingComponent);
+});
+
+const fluoriteRingComponent = {
+    onUse(event) {
+        const player = event.source // The entity that used the item on the block.
+        const act = event.itemStack // The block permutation that the item was used onw.
+        
+        if (player && act) {
+            //Minecraft.world.sendMessage("Efeito da fluorita aplicado");
+            player.runCommand("/effect @s slow_falling 5 1");
+        }
+    }
+};
+
+Minecraft.world.beforeEvents.worldInitialize.subscribe(({ itemComponentRegistry }) => {
+    itemComponentRegistry.registerCustomComponent("forge:fluorite_ring", fluoriteRingComponent);
+});
