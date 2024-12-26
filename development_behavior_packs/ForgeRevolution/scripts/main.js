9f56a084aee7623296ba8d1b6f91090762fbdb19
WaldsonLima <waldsonh@gmail.com>
2024-12-26 13:32:05 -0300

    Adicionando itens de armas e traduções

--- development_behavior_packs/ForgeRevolution/scripts/main.js
+++ development_behavior_packs/ForgeRevolution/scripts/main.js
@@ -16,18 +16,19 @@ Minecraft.world.beforeEvents.worldInitialize.subscribe(({ itemComponentRegistry 
     itemComponentRegistry.registerCustomComponent("forge:amethyst_ring", amethystRingComponent);
 });
 
-const fluoriteRingComponent = {
-    onUse(event) {
-        const player = event.source // The entity that used the item on the block.
-        const act = event.itemStack // The block permutation that the item was used onw.
+// SCRIPT PARA ADICIONAR PESO PENA AO CLIQUE DO BOTÃO DIREITO DO ANEL DE FLUORITA
+// const fluoriteRingComponent = {
+//     onUse(event) {
+//         const player = event.source // The entity that used the item on the block.
+//         const act = event.itemStack // The block permutation that the item was used onw.
         
-        if (player && act) {
-            //Minecraft.world.sendMessage("Efeito da fluorita aplicado");
-            player.runCommand("/effect @s slow_falling 5 1");
-        }
-    }
-};
+//         if (player && act) {
+//             //Minecraft.world.sendMessage("Efeito da fluorita aplicado");
+//             player.runCommand("/effect @s slow_falling 5 1");
+//         }
+//     }
+// };
 
-Minecraft.world.beforeEvents.worldInitialize.subscribe(({ itemComponentRegistry }) => {
-    itemComponentRegistry.registerCustomComponent("forge:fluorite_ring", fluoriteRingComponent);
-});
+// Minecraft.world.beforeEvents.worldInitialize.subscribe(({ itemComponentRegistry }) => {
+//     itemComponentRegistry.registerCustomComponent("forge:fluorite_ring", fluoriteRingComponent);
+// });
